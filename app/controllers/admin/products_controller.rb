a991d2e8c4738598eb9fddae657f7d45e44e0561
Scott Farmer <sfarmer664@gmail.com>
2017-02-17 00:08:53 +0000

    feature/basic auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,14 @@
 class Admin::ProductsController < ApplicationController
 
+  #http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+
   def index
     @products = Product.order(id: :desc).all
+
+    puts "--------------------------------"
+    puts ENV["ADMIN_USERNAME"]
+    puts "------------------------------"
+
   end
 
   def new
