95d7394986b5bd1ec63285969c4d290af408ac50
Scott Farmer <sfarmer664@gmail.com>
2017-02-17 14:34:28 +0000

    fixed empty cart

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,13 +1,10 @@
 class Admin::ProductsController < ApplicationController
 
-  #http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
     @products = Product.order(id: :desc).all
 
-    puts "--------------------------------"
-    puts ENV["ADMIN_USERNAME"]
-    puts "------------------------------"
 
   end
 
